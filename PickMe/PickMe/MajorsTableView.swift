956cc7b7b52e6e0f630f35afe0c35fdeae881db9
xinyuzhang <xinyuzhang@wustl.edu>
2018-11-23 21:27:37 -0500

    story board modified (adding two table views) two UITableView added

---
+++ PickMe/PickMe/MajorsTableView.swift
@@ -0,0 +1,53 @@
+//
+//  MajorsTableView.swift
+//  PickMe
+//
+//  Created by Alan zhang on 2018/11/23.
+//  Copyright © 2018 z.luo. All rights reserved.
+//
+
+// This table view aims to display the majors.
+
+import UIKit
+
+class MajorsTableView: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var majorsTable: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    
+
+}
