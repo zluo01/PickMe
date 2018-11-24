799cf5a214e2aaad2f07b12d58dd4a95980382c9
luozi01 <potterx20a@gmail.com>
2018-11-24 00:24:04 -0600

    add search courses and majors

--- PickMe/PickMe/CoursesTableView.swift
+++ PickMe/PickMe/CoursesTableView.swift
@@ -10,35 +10,49 @@
 
 import UIKit
 
-class CoursesTableView: UIViewController,UITableViewDataSource, UITableViewDelegate {
+class CoursesTableView: UIViewController,UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
     
     @IBOutlet weak var coursesTable: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
     
     var courses : [String: [String: String]] = [:]
+    var displayCourses : [String: [String: String]] = [:]
     var major : String!
     var courseSeletced : String!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         coursesTable.dataSource = self
         coursesTable.delegate = self
+        searchBar.delegate = self
+        
+        displayCourses = courses
     }
     
-//    func modifyCourses(){
-//
-//        // modify the courses array here
-//        courses.append(major) // this is for test
-//    }
-
+    func searchBar(_ searchBar: UISearchBar, textDidChange: String) {
+        self.coursesTable.reloadData()
+        
+        DispatchQueue.global(qos: .userInitiated).async {
+            if (!textDidChange.isEmpty) {
+                self.displayCourses = self.courses.filter { $0.key.uppercased().contains(textDidChange.uppercased()) ||  ($0.value["title"]?.uppercased().contains(textDidChange.uppercased()))!}
+            } else {
+                self.displayCourses = self.courses
+            }
+            DispatchQueue.main.async {
+                self.coursesTable.reloadData()
+            }
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return courses.count
+        return displayCourses.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -48,16 +62,17 @@ class CoursesTableView: UIViewController,UITableViewDataSource, UITableViewDeleg
         if myCell == nil {
             myCell = UITableViewCell(style: UITableViewCellStyle.subtitle, reuseIdentifier: "theCell")
         }
-
-        myCell?.textLabel!.text = Array(courses.keys).sorted()[indexPath.row]
+        
+        let course_id = Array(displayCourses.keys).sorted()[indexPath.row]
+        myCell?.textLabel!.text =  course_id + " - " + displayCourses[course_id]!["title"]!
         // modify here to display whether it is a required course or a selective
-//        myCell?.detailTextLabel?.text = courses[indexPath.row]
+        //        myCell?.detailTextLabel?.text = courses[indexPath.row]
         
         return myCell!
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        courseSeletced = Array(courses.keys).sorted()[indexPath.row]
+        courseSeletced = Array(displayCourses.keys).sorted()[indexPath.row]
         self.performSegue(withIdentifier: "CoursesToCourse", sender: self)
     }
     
@@ -69,13 +84,13 @@ class CoursesTableView: UIViewController,UITableViewDataSource, UITableViewDeleg
     }
     
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
--- PickMe/PickMe/MajorsTableView.swift
+++ PickMe/PickMe/MajorsTableView.swift
@@ -10,11 +10,13 @@
 
 import UIKit
 import FirebaseDatabase
-class MajorsTableView: UIViewController, UITableViewDataSource, UITableViewDelegate {
-
+class MajorsTableView: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
+    
     @IBOutlet weak var majorsTable: UITableView!
-
+    @IBOutlet weak var majorSearch: UISearchBar!
+    
     var majors = Dictionary<String, Dictionary<String, Dictionary<String, String>>>()
+    
     var majorSeletced : String!
     
     override func viewDidLoad() {
@@ -22,14 +24,18 @@ class MajorsTableView: UIViewController, UITableViewDataSource, UITableViewDeleg
         // Do any additional setup after loading the view.
         majorsTable.dataSource = self
         majorsTable.delegate = self
-        
+        majorSearch.delegate = self
         // fetch data
-        fetchDataFromFirebase()
+        fetchDataFromFirebase("")
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange: String) {
+        self.fetchDataFromFirebase(textDidChange.uppercased())
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -57,35 +63,35 @@ class MajorsTableView: UIViewController, UITableViewDataSource, UITableViewDeleg
         destination.courses = majors[majorSeletced]!
     }
     
-    func fetchDataFromFirebase(){
+    func fetchDataFromFirebase(_ keywords: String){
         let ref = Database.database().reference()
+        self.majors.removeAll()
         
         ref.observe(.value, with: {
             snapshot in
             let courses = (snapshot.value! as! Dictionary<String, Any>)["classes"] as! Dictionary<String, Dictionary<String, String>>
             for c in courses {
                 let m = c.key.split(separator: " ")[0].uppercased()
-                if self.majors.keys.contains(m) { // if major is added
-                    self.majors[m]![c.key.uppercased()] = c.value
-                } else {
-                    self.majors[m] = Dictionary<String, Dictionary<String, String>>()
-                    self.majors[m]![c.key.uppercased()] = c.value
+                if keywords.isEmpty || m.contains(keywords) {
+                    if self.majors.keys.contains(m) { // if major is added
+                        self.majors[m]![c.key.uppercased()] = c.value
+                    } else {
+                        self.majors[m] = Dictionary<String, Dictionary<String, String>>()
+                        self.majors[m]![c.key.uppercased()] = c.value
+                    }
                 }
             }
             self.majorsTable.reloadData()
         })
     }
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-    
-    
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
 }
