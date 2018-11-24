53bb81e838196adbdcb43358ae64836cd1f3e477
luozi01 <potterx20a@gmail.com>
2018-11-23 22:07:33 -0600

    comit

--- PickMe/PickMe/ViewController.swift
+++ PickMe/PickMe/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
 class ViewController: UIViewController {
 
@@ -20,6 +21,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    var group = Dictionary<String, Dictionary<String, Dictionary<String, String>>>()
+    
+    func fetchDataFromFirebase(){
+        let ref = Database.database().reference()
+        
+        ref.observe(.value, with: {
+            snapshot in
+            let courses = (snapshot.value! as! Dictionary<String, Any>)["classes"] as! Dictionary<String, Dictionary<String, String>>
+            for c in courses {
+                let m = c.key.split(separator: " ")[0].uppercased()
+                if self.group.keys.contains(m) { // if major is added
+                    self.group[m]![c.key.uppercased()] = c.value
+                } else {
+                    self.group[m] = Dictionary<String, Dictionary<String, String>>()
+                    self.group[m]![c.key.uppercased()] = c.value
+                }
+            }
+            
+        })
+    }
 }
 
