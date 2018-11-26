d4f3d31567c66fcc09961dc63d873906cbd165a5
harrywei666 <wei.xianda@wustl.edu>
2018-11-25 23:07:10 -0600

    Profile Page Half Done

--- PickMe/PickMe/UserProfileViewController.swift
+++ PickMe/PickMe/UserProfileViewController.swift
@@ -8,14 +8,51 @@
 
 import UIKit
 
-class UserProfileViewController: UIViewController {
+class UserProfileViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate{
 
+    @IBOutlet weak var myImageView: UIImageView!
     
+        @IBOutlet weak var nameTextView: UITextView!
+        @IBOutlet weak var majorTextView: UITextView!
+        @IBOutlet weak var minorTextView: UITextView!
+    
+    @IBAction func importImage(_ sender: AnyObject){
+        let image = UIImagePickerController()
+        image.delegate = self
+        image.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        image.allowsEditing = true
+        self.present(image, animated: true){
+            //After it is complete
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage{
+            myImageView.image = image
+        }
+        else{
+            //Error message
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        nameTextView.text = "Joseph Halom"
+        nameTextView.font = UIFont.boldSystemFont(ofSize: 30)
+        nameTextView.isEditable = true
+        nameTextView.autocapitalizationType = UITextAutocapitalizationType.allCharacters
         
-
+        majorTextView.text = "Computer Science"
+        majorTextView.font = UIFont(name: "Verdana", size: 17)
+        majorTextView.isEditable = true
+        majorTextView.autocapitalizationType = UITextAutocapitalizationType.allCharacters
+        
+        minorTextView.text = "French"
+        minorTextView.font = UIFont(name: "Verdana", size: 17)
+        minorTextView.isEditable = true
+        minorTextView.autocapitalizationType = UITextAutocapitalizationType.allCharacters
+        
         // Do any additional setup after loading the view.
     }
 
