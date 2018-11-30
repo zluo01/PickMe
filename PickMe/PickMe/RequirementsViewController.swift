977ae645f08728b427c175856c6b0b1230929c56
luozi01 <potterx20a@gmail.com>
2018-11-30 05:19:01 -0600

    fix preference search, half fix single course view, left with couple method to implements

--- PickMe/PickMe/RequirementsViewController.swift
+++ PickMe/PickMe/RequirementsViewController.swift
@@ -14,16 +14,24 @@ class RequirementsViewController: UIViewController, UITableViewDataSource,  UITa
     @IBOutlet var filterSearchBar: UISearchBar!
     @IBOutlet var majorTable: UITableView!
     
-    var allMajors = [String]()
-    var allMajorsLink = [String]()
+    var majorsMinors : [String: String] = [:]
     
-    var filteredMajors = [String]()
-    var filteredMajorsLink = [String]()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        majorTable.dataSource = self
+        majorTable.delegate = self
+        filterSearchBar.delegate = self
+        fetchDataFromFirebase("")
+        // Do any additional setup after loading the view.
+    }
     
-    var filtered = false
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return filtered ? filteredMajors.count :allMajors.count
+        return majorsMinors.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -33,73 +41,34 @@ class RequirementsViewController: UIViewController, UITableViewDataSource,  UITa
             myCell = UITableViewCell(style: UITableViewCellStyle.subtitle, reuseIdentifier: "theCell")
         }
         
-        myCell?.textLabel!.text = filtered ? filteredMajors[indexPath.row] : allMajors[indexPath.row]
+        myCell?.textLabel!.text = Array(majorsMinors.keys).sorted()[indexPath.row]
         return myCell!
-    }
-    
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        majorTable.dataSource = self
-        majorTable.delegate = self
-        filterSearchBar.delegate = self
-        fetchDataFromDatabase()
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let wvcontroller = WebViewController()
-        wvcontroller.urlString = filtered ? filteredMajorsLink[indexPath.row] : allMajorsLink[indexPath.row]
+        wvcontroller.urlString = majorsMinors[Array(majorsMinors.keys).sorted()[indexPath.row]]
         self.navigationController?.pushViewController(wvcontroller, animated: true)
     }
 
-    func fetchDataFromDatabase() {
+    func fetchDataFromFirebase(_ keywords: String) {
         let ref = Database.database().reference()
+        self.majorsMinors.removeAll()
         
         ref.observe(.value, with: {
             snapshot in
             let requirements = (snapshot.value! as! Dictionary<String, Any>)["requirements"] as! Array<Dictionary<String, String>>
             for i in requirements {
-                if (i["name"] != nil && i["link"] != nil) {
-                    self.allMajors.append(i["name"]!)
-                    self.allMajorsLink.append(i["link"]!)
+                let m = i["name"]!.uppercased()
+                if keywords.isEmpty || m.contains(keywords) {
+                    self.majorsMinors[i["name"]!] = i["link"]!
                 }
             }
-            print(self.allMajors.count)
-            print(self.allMajors[0])
             self.majorTable.reloadData()
         })
     }
     
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        filteredMajors = allMajors.filter({$0.prefix(searchText.count) == searchText})
-        filteredMajorsLink = []
-        for i in filteredMajors {
-            let index = allMajors.index(of: i)
-            filteredMajorsLink.append(allMajorsLink[index!])
-        }
-        filtered = true
-        majorTable.reloadData()
+    func searchBar(_ searchBar: UISearchBar, textDidChange : String) {
+       self.fetchDataFromFirebase(textDidChange.uppercased())
     }
-    
-    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
-        filtered = false
-        searchBar.text! = ""
-        majorTable.reloadData()
-    }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- PickMe/PickMe/SingleCourseDetails.swift
+++ PickMe/PickMe/SingleCourseDetails.swift
@@ -9,9 +9,11 @@
 import UIKit
 import FirebaseDatabase
 import FirebaseAuth
+import ExpandingMenu
 
 class SingleCourseDetails: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     let semesters = ["1","2","3","4","5","6","7","8"]
+    
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
@@ -19,24 +21,32 @@ class SingleCourseDetails: UIViewController, UIPickerViewDelegate, UIPickerViewD
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return semesters.count
     }
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return semesters[row]
+    
+    func pickerView(_ pickerView: UIPickerView, attributedTitleForRow row: Int, forComponent component: Int) -> NSAttributedString? {
+        let attributedString = NSAttributedString(string: semesters[row], attributes: [NSAttributedStringKey.foregroundColor : UIColor.white])
+        return attributedString
     }
+    
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        semesterField.text = semesters[row]
+        self.selectedText = semesters[row]
     }
     
     @IBOutlet weak var courseId: UILabel!
     @IBOutlet weak var courseTitle: UILabel!
     @IBOutlet weak var courseDes: UILabel!
     @IBOutlet weak var scrollView: UIScrollView!
-    @IBOutlet var semesterField: UITextField!
-    @IBOutlet var addClassButton: UIButton!
-    //var course : String!
+    
+    @IBOutlet weak var PickView: UIView!
+    @IBOutlet weak var picker: UIPickerView!
+    
+    var selectedText = String()
     var details : [String: String]!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.addSubview(PickView)
+        PickView.isHidden = true
+        
         // Do any additional setup after loading the view.
         courseId.text = details["course_code"]
         courseTitle.text = details["title"]
@@ -46,74 +56,114 @@ class SingleCourseDetails: UIViewController, UIPickerViewDelegate, UIPickerViewD
         courseTitle.sizeToFit()
         courseDes.sizeToFit()
         
-        scrollView.contentLayoutGuide.bottomAnchor.constraint(equalTo: courseDes.bottomAnchor).isActive = true
-        createPicker()
-    }
-    
-    func createPicker() {
-        let picker = UIPickerView()
+        // UIPickerView
+        picker.showsSelectionIndicator = true
+        picker.alpha = 0.85
+        
         picker.delegate = self
         picker.dataSource = self
-        semesterField.inputView = picker
+        
+        scrollView.contentLayoutGuide.bottomAnchor.constraint(equalTo: courseDes.bottomAnchor).isActive = true
+        
+        selectedText = semesters[0]
+        configureExpandingMenuButton()
+    }
+    
+    @IBAction func cancelAction(_ sender: UIButton) {
+        PickView.isHidden = true
     }
 
     @IBAction func addClassAction(_ sender: UIButton) {
-        if semesterField.text == "" || semesterField.text == nil {
-            let alertController = UIAlertController(title: "Empty Field", message: "Please indicate in which semester of your 8 semesters you took this class", preferredStyle: .alert)
-            let cancelAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-            alertController.addAction(cancelAction)
-            self.present(alertController, animated: true, completion: nil)
-        }
-        else {
-            addClass(courseId.text!, semesterField.text!)
-        }
+        print(selectedText)
+        addClass(courseId.text!, selectedText)
     }
+    
     func addClass(_ courseCode:String, _ semester:String) {
         print("Called addClass")
+        // Todo add userDefault for guest users
+        // no matter what should add to user default
+        print("Add to user default")
+        // add default code here
+        
+        // if user is loged in, then also add to database
         if Auth.auth().currentUser != nil {
-           
-            semesterField.resignFirstResponder()
             let ref = Database.database().reference()
             
             ref.observe(.value, with: {
                 snapshot in
                 let users = (snapshot.value! as! Dictionary<String, Any>)["users"] as! Dictionary<String, Dictionary<String, String>>
                 let currentUser = Auth.auth().currentUser!
-                if users.keys.contains(currentUser.uid) {
-                    var userDict = users[currentUser.uid]!
-                    var added = false
-                    if userDict.keys.contains(courseCode) && userDict[courseCode] != semester{
-                        print("In if")
-                        let alertController = UIAlertController(title: "Class Already Added", message: "You have already added this class.", preferredStyle: .alert)
-                        let cancelAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-                        alertController.addAction(cancelAction)
-                        self.present(alertController, animated: true, completion: nil)
-                    }
-                    else {
-                        print("In else")
-                        userDict[courseCode] = semester
-                        ref.child("users").child(currentUser.uid).setValue(userDict)
-                        added = true
-                    }
-                }
-                else {
-                    var newDict = Dictionary<String, String>()
-                    newDict[courseCode] = semester
-                    ref.child("users").child(currentUser.uid).setValue(newDict)
-                }
+                print(users)
+                print(currentUser)
+//                if users.keys.contains(currentUser.uid) {
+//                    var userDict = users[currentUser.uid]!
+//                    var added = false
+//                    if userDict.keys.contains(courseCode) && userDict[courseCode] != semester{
+//                        print("In if")
+//                        let alertController = UIAlertController(title: "Class Already Added", message: "You have already added this class.", preferredStyle: .alert)
+//                        let cancelAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+//                        alertController.addAction(cancelAction)
+//                        self.present(alertController, animated: true, completion: nil)
+//                    }
+//                    else {
+//                        print("In else")
+//                        userDict[courseCode] = semester
+//                        ref.child("users").child(currentUser.uid).setValue(userDict)
+//                        added = true
+//                    }
+//                }
+//                else {
+//                    var newDict = Dictionary<String, String>()
+//                    newDict[courseCode] = semester
+//                    ref.child("users").child(currentUser.uid).setValue(newDict)
+//                }
             })
         }
-        else {
-            let alertController = UIAlertController(title: "Operation Not Allowed", message: "You have to log in first.", preferredStyle: .alert)
-            let cancelAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-            alertController.addAction(cancelAction)
-            self.present(alertController, animated: true, completion: nil)
-            
-        }
+        PickView.isHidden = true
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    //Todo calculate the recommendation
+    func recommend() {
+        
+    }
+    
+    fileprivate func configureExpandingMenuButton() {
+        let menuButtonSize: CGSize = CGSize(width: 64.0, height: 64.0)
+        let menuButton = ExpandingMenuButton(frame: CGRect(origin: CGPoint.zero, size: menuButtonSize), image: UIImage(named: "chooser-button-tab")!, rotatedImage: UIImage(named: "chooser-button-tab-highlighted")!)
+        menuButton.center = CGPoint(x: self.view.bounds.width - 32.0, y: self.view.bounds.height - 72.0)
+        self.view.addSubview(menuButton)
+        
+        let favor = ExpandingMenuItem(size: menuButtonSize, title: "Favor", image: UIImage(named: "heart")!, highlightedImage: UIImage(named: "heart-highlight")!, backgroundImage: UIImage(named: "chooser-moment-button"), backgroundHighlightedImage: UIImage(named: "chooser-moment-button-highlighted")) { () -> Void in
+            favorClass()
+        }
+        
+        let check = ExpandingMenuItem(size: menuButtonSize, title: "Check", image: UIImage(named: "check")!, highlightedImage: UIImage(named: "check-highlight")!, backgroundImage: UIImage(named: "chooser-moment-button"), backgroundHighlightedImage: UIImage(named: "chooser-moment-button-highlighted")) { () -> Void in
+            checkClass()
+        }
+        
+        func checkClass() {
+            PickView.isHidden = false
+        }
+        
+        func favorClass() {
+            // add to user default
+            
+            //add to database if log in
+        }
+        
+        menuButton.addMenuItems([favor, check])
+        
+        menuButton.willPresentMenuItems = { (menu) -> Void in
+            print("MenuItems will present.")
+        }
+        
+        menuButton.didDismissMenuItems = { (menu) -> Void in
+            print("MenuItems dismissed.")
+        }
     }
 }
