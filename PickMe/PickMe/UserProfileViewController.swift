c059f822a025aa90fa5b80511fb5fba55b5ebb8c
luozi01 <potterx20a@gmail.com>
2018-11-30 15:48:04 -0600

    fix profile logic

--- PickMe/PickMe/UserProfileViewController.swift
+++ PickMe/PickMe/UserProfileViewController.swift
@@ -9,85 +9,93 @@
 import UIKit
 import FirebaseAuth
 import FirebaseDatabase
+import ExpandingMenu
 
 class UserProfileViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate{
 
     @IBOutlet weak var myImageView: UIImageView!
     
-    @IBOutlet weak var nameTextView: UITextView!
-
-    @IBOutlet weak var majorTextView: UITextView!
-
-    @IBOutlet weak var minorTextView: UITextView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var firstMajorLabel: UILabel!
+    @IBOutlet weak var secondMajorLabel: UILabel!
+    @IBOutlet weak var userActionButton: UIBarButtonItem!
     
-    @IBAction func importImage(_ sender: UIButton) {
-        let image = UIImagePickerController()
-        image.delegate = self
-        image.sourceType = UIImagePickerControllerSourceType.photoLibrary
-        image.allowsEditing = true
-        self.present(image, animated: true){
-            //After it is complete
-        }
-    }
-    
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage{
-            myImageView.image = image
-        }
-        else{
-            //Error message
-        }
-        self.dismiss(animated: true, completion: nil)
-    }
+    //    @IBAction func importImage(_ sender: UIButton) {
+//        let image = UIImagePickerController()
+//        image.delegate = self
+//        image.sourceType = UIImagePickerControllerSourceType.photoLibrary
+//        image.allowsEditing = true
+//        self.present(image, animated: true){
+//            //After it is complete
+//        }
+//    }
+//
+//    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+//        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage{
+//            myImageView.image = image
+//        }
+//        else{
+//            //Error message
+//        }
+//        self.dismiss(animated: true, completion: nil)
+//    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        nameTextView.text = "Jon Halom"
-        nameTextView.font = UIFont.boldSystemFont(ofSize: 22)
-        nameTextView.isEditable = true
-        nameTextView.autocapitalizationType = UITextAutocapitalizationType.allCharacters
         
-        majorTextView.text = "Computer Science"
-        majorTextView.font = UIFont(name: "Verdana", size: 17)
-        majorTextView.isEditable = true
-        majorTextView.autocapitalizationType = UITextAutocapitalizationType.allCharacters
-        
-        minorTextView.text = "French"
-        minorTextView.font = UIFont(name: "Verdana", size: 17)
-        minorTextView.isEditable = true
-        minorTextView.autocapitalizationType = UITextAutocapitalizationType.allCharacters
-        
-        // Do any additional setup after loading the view.
+        if isGuest() {
+            userActionButton.title = "Login"
+        } else { // Todo double check here
+            userActionButton.title = "Logout"
+        }
+        configureExpandingMenuButton()
     }
     
-    @IBAction func logout(_ sender: Any) {
-        let firebaseAuth = Auth.auth()
-        do {
-            try firebaseAuth.signOut()
-            let alertController = UIAlertController(title: "Logout Success", message: "You have successfully logged out!", preferredStyle: .alert)
-            let cancelAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-            alertController.addAction(cancelAction)
-            self.present(alertController, animated: true, completion: nil)
-            print("You have logged out!")
-        } catch let signOutError as NSError {
-            print ("Error signing out: %@", signOutError)
+    //Todo implement
+    @IBAction func tableSwitch(_ sender: UISegmentedControl) {
+    }
+    
+    // Todo implement login logout
+    @IBAction func userActions(_ sender: Any) {
+        if isGuest() {
+            // go to login page
+        } else { // logout here
+            let firebaseAuth = Auth.auth()
+            do {
+                try firebaseAuth.signOut()
+                let alertController = UIAlertController(title: "Logout Success", message: "You have successfully logged out!", preferredStyle: .alert)
+                let cancelAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                alertController.addAction(cancelAction)
+                self.present(alertController, animated: true, completion: nil)
+                print("You have logged out!")
+            } catch let signOutError as NSError {
+                print ("Error signing out: %@", signOutError)
+            }
         }
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    fileprivate func configureExpandingMenuButton() {
+        let menuButtonSize: CGSize = CGSize(width: 64.0, height: 64.0)
+        let menuButton = ExpandingMenuButton(frame: CGRect(origin: CGPoint.zero, size: menuButtonSize), image: UIImage(named: "chooser-button-tab")!, rotatedImage: UIImage(named: "chooser-button-tab-highlighted")!)
+        menuButton.center = CGPoint(x: self.view.bounds.width - 32.0, y: self.view.bounds.height - 72.0)
+        self.view.addSubview(menuButton)
+        
+        let edit = ExpandingMenuItem(size: menuButtonSize, title: "Edit", image: UIImage(named: "heart")!, highlightedImage: UIImage(named: "heart-highlight")!, backgroundImage: UIImage(named: "chooser-moment-button"), backgroundHighlightedImage: UIImage(named: "chooser-moment-button-highlighted")) { () -> Void in
+            // add action here
+        }
+        
+        menuButton.addMenuItems([edit])
+        
+        menuButton.willPresentMenuItems = { (menu) -> Void in
+            print("MenuItems will present.")
+        }
+        
+        menuButton.didDismissMenuItems = { (menu) -> Void in
+            print("MenuItems dismissed.")
+        }
     }
-    */
-
 }
